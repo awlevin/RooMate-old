4a7cba80e6fd0534494971b1413e9c6e98ae3a24
Ritvik Upadhyaya <ritvikupadhyaya@gmail.com>
2016-10-19 14:56:16 -0500

    Added a struct for RMBulletinPost

---
+++ roomate/RMClasses/RMBulletinPost.swift
@@ -0,0 +1,25 @@
+//
+//  RMBulletinPost.swift
+//  roomate
+//
+//  Created by Ritvik Upadhyaya on 19/10/16.
+//  Copyright © 2016 RooMate. All rights reserved.
+//
+
+import Foundation
+
+struct RMBulletinPost {
+    var objectId: String
+    var dateCreatedAt: String
+    var dateupdatedAt: String
+    var title: String
+    var description: String
+    var pinNote: Bool
+    var photos: [String]
+    var thumbnail: String
+    var daysBeforeRemoval: Int
+    var comments: [String: String]
+    
+    
+    
+}
