f3857add00632bf32b5b38df4632b34898338735
Ritvik Upadhyaya <ritvikupadhyaya@gmail.com>
2016-11-07 15:42:22 -0600

    Fixed UI Issues and updated the UI for image upload and the inputs

--- roomate/Bulletin Board/createBBPostVC.swift
+++ roomate/Bulletin Board/createBBPostVC.swift
@@ -8,12 +8,13 @@
 
 import UIKit
 
-class createBBPostVC: UIViewController, UITextViewDelegate,UICollectionViewDelegate, UICollectionViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class createBBPostVC: UIViewController, UITextViewDelegate,UICollectionViewDelegate, UICollectionViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
     var defaultOrangeColor = UIColor(red: 232.0, green: 128.0, blue: 50.0, alpha: 1.0)
     
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var imageCollectionView: UICollectionView!
+    @IBOutlet weak var titleTextField: UITextField!
     
     var imagePicker = UIImagePickerController()
     var imageAddedCollection : [UIImage] = []
@@ -22,12 +23,18 @@ class createBBPostVC: UIViewController, UITextViewDelegate,UICollectionViewDeleg
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         descriptionTextView.delegate = self
+        titleTextField.delegate = self
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(createBBPostVC.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(createBBPostVC.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
         
         imageCollectionView.registerNib(UINib(nibName: "DetailBBPhotoCell", bundle: nil), forCellWithReuseIdentifier: collectionCellIdentifier)
         
         descriptionTextView.text = "Enter your post description here"
         descriptionTextView.textColor = UIColor.lightGrayColor()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -88,9 +95,55 @@ class createBBPostVC: UIViewController, UITextViewDelegate,UICollectionViewDeleg
     }
     
     func textViewDidEndEditing(textView: UITextView) {
+        textView.text = textView.text.stringByTrimmingCharactersInSet(
+            NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        )
         if textView.text.isEmpty {
             textView.text = "Enter your post description here"
             textView.textColor = UIColor.lightGrayColor()
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        textField.text = textField.text!.stringByTrimmingCharactersInSet(
+            NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        )
+        if !(textField.text?.isEmpty)!{
+            title = textField.text
+        } else {
+            title = "New Bulletin Post"
+        }
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            if view.frame.origin.y == 64 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+            else {
+            
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue() {
+            if view.frame.origin.y != 64 {
+                self.view.frame.origin.y += keyboardSize.height
+            }
+            else {
+            
+            }
         }
     }
     
