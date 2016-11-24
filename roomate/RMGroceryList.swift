37ada77d38ca169335a77b1a68b95493a19587f5
Aaron Levin <awlevin@comcast.net>
2016-11-23 20:12:13 -0600

    added backend integration for model classes RMGrocery and RMGroceryList

--- roomate/RMGroceryList.swift
+++ roomate/RMGroceryList.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+enum RMGroceryListTypes {
+    case Personal
+    case Communal
+    case Aggregate
+}
+
 public struct RMGroceryList {
     var objectId: String
     var dateCreatedAt: String
@@ -18,17 +24,95 @@ public struct RMGroceryList {
     
     
     
-    // Public Functions
-    /*
-    public func getCurrentUserGroceryList(userObjectId: String) -> RMGroceryList {
     
+    // Public Functions
+    
+    static func getGroceryList(userObjectId: String, lastid: Int, groupId: Int, listType: RMGroceryListTypes, completionHandler: (bbPosts: [RMGrocery])->()) {
+        
+        var backendURLpostfix: String
+        
+        switch(listType) {
+        case RMGroceryListTypes.Personal:
+            backendURLpostfix = "selectRMUserGroceries"
+        case RMGroceryListTypes.Communal:
+            backendURLpostfix = "selectRMCommunalGroceries"
+        case RMGroceryListTypes.Aggregate:
+            backendURLpostfix = "selectRMAggregateGroceries"
+        }
+        
+        
+        
+        let apiCallString = "https://damp-plateau-63440.herokuapp.com/\(backendURLpostfix)"
+        let httpURL = NSURL(string: apiCallString)
+        let request = NSMutableURLRequest(URL: httpURL!)
+        
+        request.HTTPMethod = "GET"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("1", forHTTPHeaderField: "groupid")
+        request.addValue("\(lastid)", forHTTPHeaderField: "lastid")
+        
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: configuration, delegate: nil, delegateQueue: nil)
+        
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            var statusCode = 0
+            if let httpResponse = response as? NSHTTPURLResponse {
+                statusCode = httpResponse.statusCode
+            }
+            
+            if(error != nil || data == nil || statusCode != 200){
+                switch statusCode {
+                case 400:
+                    completionHandler(bbPosts: [])
+                    break
+                default:
+                    completionHandler(bbPosts: [])
+                    break
+                }
+                return
+            } else {
+                var json: NSArray
+                do {
+                    try json = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions()) as! NSArray
+                } catch {
+                    completionHandler(bbPosts: [])
+                    return
+                }
+                
+                if json.count == 0 {
+                    completionHandler(bbPosts: [] )
+                    return
+                }
+                else {
+                    var returnedGroceryItems = [RMGrocery]()
+                    
+                    for jsonItem in json {
+                        guard let jsonItemDict = jsonItem as? [String: AnyObject]
+                            else { continue }
+                        
+                        let currItem = RMGrocery(objectId: jsonItemDict["itemid"] as! Int,
+                                                 userId: jsonItemDict["userid"] as! Int,
+                                                 groupId: jsonItemDict["groupid"] as! Int,
+                                                 isPersonalItem: jsonItemDict["personalitem"] as! Bool,
+                                                 dateCreatedAt: jsonItemDict["datecreatedat"] as! String,
+                                                 dateUpdatedAt: jsonItemDict["dateupdatedat"] as! String,
+                                                 groceryItemName: jsonItemDict["groceryitemname"] as! String,
+                                                 groceryItemPrice: jsonItemDict["groceryitemprice"] as! Double,
+                                                 groceryItemDescription: jsonItemDict["groceryitemdescription"] as! String)
+                        returnedGroceryItems.append(currItem)
+                    }
+                    completionHandler(bbPosts: returnedGroceryItems)
+                    return
+                }
+            }
+        }
+        task.resume()
     }
+    
+    /*
+    
     
     public func getPastUserGroceryList(offset: Int, count: Int) -> [RMGroceryList] {
-        
-    }
-    
-    public func getAggregateGroceries(groupId: String) -> RMGroceryList{
         
     }
     
@@ -66,11 +150,6 @@ public struct RMGroceryList {
     private func changeLastUpdateDate() {
         
     }
-    
-    private func compileAggregateGroceryList(groupId: String) -> [RMGroceryList] {
-        
-    }
-    
     
     */
 }
